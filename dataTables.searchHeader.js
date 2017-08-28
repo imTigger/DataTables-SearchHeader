0461d9641da8e2817a3da1ea1d44b49ebe5b5f4c
Tiger Fok <tiger.indzz@gmail.com>
2017-08-28 18:41:37 +0800

    Fixed: column initially hidden not searchable when shown

--- dataTables.searchHeader.js
+++ dataTables.searchHeader.js
@@ -1,42 +1,48 @@
 $(document).on('init.dt', function (e, settings, json) {
     var $table = $(settings.nTable);
 
-    if (!$table.hasClass('search-header') && !$table.hasClass('search-header')) {
+    if (!$table.hasClass('search-header')) {
         return;
     }
 
     // Copy thead to tfoot
-    $table.append($('<tfoot/>').append($($table.find("thead tr").clone())));
+    $tfoot = $('<tfoot/>');
 
     // Append fields
-    $table.find("tfoot th").each(function (k, v) {
+    for (k in settings.aoColumns) {
         var column = settings.aoColumns[k];
+        var $th = $('<th/>');
+        
+        $tfoot.append($th);
+        
         if (!column.bSearchable) {
-            $(this).html('');
+            $th.html('');
         } else if (column.searchType == 'select' && Object.keys(column.searchOptions).length > 0) {
-            $(this).html('<select style="width: 100%" class="datatable-search-field simple-search-field select-search-field"></select>');
-            $(this).find('select').append('<option value=""></option>');
+            $th.html('<select style="width: 100%" class="datatable-search-field simple-search-field select-search-field"></select>');
+            $th.find('select').append('<option value=""></option>');
 
             for (var i in column.searchOptions) {
-                $(this).find('select').append('<option value="' + column.searchOptions[i].key + '">' + column.searchOptions[i].value + '</option>');
+                $th.find('select').append('<option value="' + column.searchOptions[i].key + '">' + column.searchOptions[i].value + '</option>');
             }
         } else if (column.searchType == 'date') {
-            $(this).html('<input type="text" class="datatable-search-field simple-search-field date-search-field datepicker"/>');
+            $th.html('<input type="text" class="datatable-search-field simple-search-field date-search-field datepicker"/>');
         } else if (column.searchType == 'date-range') {
-            $(this).html('<input type="text" placeholder="From" class="datatable-search-field date-range-search-field datepicker datatable-filter" name="' + column.data + '_from"/><input type="text" placeholder="To" class="date-range-search-field datepicker datatable-filter" name="' + column.data + '_to"/>');
+            $th.html('<input type="text" placeholder="From" class="datatable-search-field date-range-search-field datepicker datatable-filter" name="' + column.data + '_from"/><input type="text" placeholder="To" class="date-range-search-field datepicker datatable-filter" name="' + column.data + '_to"/>');
         } else if (column.searchType == 'datetime') {
-            $(this).html('<input type="text" class="datatable-search-field simple-search-field datetime-search-field datetimepicker"/>');
+            $th.html('<input type="text" class="datatable-search-field simple-search-field datetime-search-field datetimepicker"/>');
         } else if (column.searchType == 'datetime-range') {
-            $(this).html('<input type="text" placeholder="From" class="datatable-search-field datetime-range-search-field datetimepicker datatable-filter" name="' + column.data + '_from"/><input type="text" placeholder="To" class="datetime-range-search-field datetimepicker datatable-filter" name="' + column.data + '_to"/>');
+            $th.html('<input type="text" placeholder="From" class="datatable-search-field datetime-range-search-field datetimepicker datatable-filter" name="' + column.data + '_from"/><input type="text" placeholder="To" class="datetime-range-search-field datetimepicker datatable-filter" name="' + column.data + '_to"/>');
         } else {
-            $(this).html('<input type="text" class="datatable-search-field simple-search-field text-search-field"/>');
+            $th.html('<input type="text" class="datatable-search-field simple-search-field text-search-field"/>');
         }
 
         // Hide column if not visible
         if (!column.bVisible) {
-            $(this).hide();
+            $th.hide();
         }
-    });
+    };
+    
+    $table.append($tfoot);
 
     var dt = $table.dataTable().api();
 
