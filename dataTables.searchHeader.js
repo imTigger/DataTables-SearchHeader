a8f6f01bf23785a2a088549e567907aea0a61eb2
Tiger Fok <tiger.indzz@gmail.com>
2017-09-01 17:43:50 +0800

    Cleanup

--- dataTables.searchHeader.js
+++ dataTables.searchHeader.js
@@ -12,28 +12,29 @@ $(document).on('init.dt', function (e, settings, json) {
     for (k in settings.aoColumns) {
         var column = settings.aoColumns[k];
         var $th = $('<th/>');
+        var fieldName = column.data;
         
         $tfoot.append($th);
         
         if (!column.bSearchable) {
             $th.html('');
         } else if (column.searchType == 'select' && Object.keys(column.searchOptions).length > 0) {
-            $th.html('<select style="width: 100%" class="datatable-search-field simple-search-field select-search-field" name="' + column.data + '"></select>');
+            $th.html('<select style="width: 100%" class="datatable-search-field simple-search-field select-search-field" name="' + fieldName + '"></select>');
             $th.find('select').append('<option value=""></option>');
 
             for (var i in column.searchOptions) {
                 $th.find('select').append('<option value="' + column.searchOptions[i].key + '">' + column.searchOptions[i].value + '</option>');
             }
         } else if (column.searchType == 'date') {
-            $th.html('<input type="text" class="datatable-search-field simple-search-field date-search-field datepicker" name="' + column.data + '"/>');
+            $th.html('<input type="text" class="datatable-search-field simple-search-field date-search-field datepicker" name="' + fieldName + '"/>');
         } else if (column.searchType == 'date-range') {
-            $th.html('<input type="text" placeholder="From" class="datatable-search-field date-range-search-field datepicker datatable-filter" name="' + column.data + '_from"/><input type="text" placeholder="To" class="date-range-search-field datepicker datatable-filter" name="' + column.data + '_to"/>');
+            $th.html('<input type="text" placeholder="From" class="datatable-search-field date-range-search-field datepicker datatable-filter" name="' + fieldName + '_from"/><input type="text" placeholder="To" class="date-range-search-field datepicker datatable-filter" name="' + fieldName + '_to"/>');
         } else if (column.searchType == 'datetime') {
             $th.html('<input type="text" class="datatable-search-field simple-search-field datetime-search-field datetimepicker"/>');
         } else if (column.searchType == 'datetime-range') {
-            $th.html('<input type="text" placeholder="From" class="datatable-search-field datetime-range-search-field datetimepicker datatable-filter" name="' + column.data + '_from"/><input type="text" placeholder="To" class="datetime-range-search-field datetimepicker datatable-filter" name="' + column.data + '_to"/>');
+            $th.html('<input type="text" placeholder="From" class="datatable-search-field datetime-range-search-field datetimepicker datatable-filter" name="' + fieldName + '_from"/><input type="text" placeholder="To" class="datetime-range-search-field datetimepicker datatable-filter" name="' + fieldName + '_to"/>');
         } else {
-            $th.html('<input type="text" class="datatable-search-field simple-search-field text-search-field" name="' + column.data + '"/>');
+            $th.html('<input type="text" class="datatable-search-field simple-search-field text-search-field" name="' + fieldName + '"/>');
         }
 
         // Hide column if not visible
